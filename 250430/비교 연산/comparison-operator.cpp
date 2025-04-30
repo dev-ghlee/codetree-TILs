5c5c6191709395afeab63820f22f99a648d9e4e5
Lee Gihyun <59757905+dev-ghlee@users.noreply.github.com>
2025-04-30 22:45:28 +0900

    Title: 비교 연산, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

--- 250430/비교 연산/comparison-operator.cpp
+++ 250430/비교 연산/comparison-operator.cpp
@@ -4,8 +4,12 @@ using namespace std;
 int main() {
     int A, B;
     cin >> A >> B;
-    cout << (A>+B) << "\n";
-    cout << (A>+B) << "\n";
-    cout << (A>+B) << "\n";
+    cout << (A >= B) << "\n";
+    cout << (A > B) << "\n";
+    cout << (B >= A ) << "\n";
+    cout << (B > A) << "\n";
+    cout << (A == B) << "\n";
+    cout << (A != B) ;
+
     return 0;
 }
