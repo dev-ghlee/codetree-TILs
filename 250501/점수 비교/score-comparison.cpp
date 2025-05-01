e1716af3fe9997af6fe456612ac9566e07b4ea60
Lee Gihyun <59757905+dev-ghlee@users.noreply.github.com>
2025-05-01 23:37:42 +0900

    Title: 점수 비교, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250501/점수 비교/score-comparison.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int a_m, a_e , b_m , b_e;
+    cin >> a_m >> a_e >> b_m >> b_e;
+
+    if((a_m > b_m) && (a_e > b_e))
+        cout << "1";
+    else
+        cout << "0";
+    
+
+    return 0;
+}
