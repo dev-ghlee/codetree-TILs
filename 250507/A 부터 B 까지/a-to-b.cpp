41317d3d777d2462819f1094d0adc2dedf3606a4
Lee Gihyun <59757905+dev-ghlee@users.noreply.github.com>
2025-05-07 00:05:26 +0900

    Title: A 부터 B 까지, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250507/A 부터 B 까지/a-to-b.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a,b;
+    cin >> a >> b;
+    cout << a <<" ";
+    while(true)
+    {
+        if(a %2 == 0 ) 
+            a +=3;
+        else
+            a *=2;
+        if(a >b)
+            break;
+        cout << a << " ";
+    }
+    return 0;
+}
