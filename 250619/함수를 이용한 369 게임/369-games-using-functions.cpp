920ccfe21d6f5c56b03006504d37ac43f81012ba
Lee Gihyun <59757905+dev-ghlee@users.noreply.github.com>
2025-06-19 23:28:15 +0900

    Title: 함수를 이용한 369 게임, Time: 9ms, Memory: 0MB, Status: Passed - Codetree

--- 250619/함수를 이용한 369 게임/369-games-using-functions.cpp
+++ 250619/함수를 이용한 369 게임/369-games-using-functions.cpp
@@ -2,12 +2,25 @@
 
 using namespace std;
 
+bool IsValid(int i)
+{
+    if(i%3 == 0 ) return true;
+
+    while(i > 0)
+    {
+        if((i%10) != 0 &&(i%10) %3 == 0 ) return true;
+        i /= 10;
+    }
+
+    return false;
+}
+
 int CheckNum(int a, int b )
 {
     int cnt = 0;
     for(int i = a; i <=b; i++)
     {
-        if(i % 3 == 0 || (i / 10)% 3 == 0 || (i%10) % 3 == 0)
+        if(IsValid(i))
             cnt++;
     }
     return cnt;
